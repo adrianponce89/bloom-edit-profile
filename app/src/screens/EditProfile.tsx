84b247194a3af31d3f3f37c5dc98a8f1fd7e3e15
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-10-12 15:37:44 -0300

    Add project routes

---
+++ app/src/screens/EditProfile.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PhotoList from '../components/PhotoList';
+
+const EditProfile: () => React.Node = () => {
+  return <PhotoList />;
+};
+
+export default EditProfile;
